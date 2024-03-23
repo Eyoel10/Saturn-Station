ea6d6b7365bf2584aef7b527d0ab6fb372a0fb37
Julian Koksal <julian.koksal@gmail.com>
2024-03-22 21:41:32 -0400

    Asteroids now reset when out of bounds.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -8,25 +8,19 @@ public class Asteroid : MonoBehaviour
     public Vector3 heading;
     public bool isReadyToSpawn;
 
-    float xMin;
-
-    void Start()
-    {
-        xMin = -Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f)).x;
-    }
+    static Bounds bounds = new(new Vector3(), new Vector3(40.0f, 40.0f));
 
     void Update()
     {
         transform.Translate(speed * Time.deltaTime * heading, Space.World);
         transform.Rotate(0.0f, 0.0f, rotationSpeed * Time.deltaTime);
-        if (transform.position.x < xMin)
-        {
-            isReadyToSpawn = true;
-            gameObject.SetActive(false);
-        }
+        if (!bounds.Contains(transform.position))
+            ResetAsteroid();
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision) => ResetAsteroid();
+
+    void ResetAsteroid()
     {
         isReadyToSpawn = true;
         gameObject.SetActive(false);
